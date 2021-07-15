342ad49b7b079b29e2e1d5eefecafe69cb60dcdb
ʝօӄɛʀ <83216347+imvijayps@users.noreply.github.com>
2021-07-15 09:43:46 +0530

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -1,6 +1,4 @@
-from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import CommandHandler
-from bot import FSUB_ENABLED, FSUB_CHANNEL_ID, FSUB_CHANNEL_LINK, SUDO_USERS, OWNER_ID
 from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
 from bot.helper.telegram_helper.message_utils import *
 from bot.helper.telegram_helper.filters import CustomFilters
@@ -11,25 +9,6 @@ from bot import dispatcher
 
 def cloneNode(update,context):
     args = update.message.text.split(" ",maxsplit=1)
-    
-    user_id = update.effective_user.id
-
-    user_is_normal = True
-    if (user_id == OWNER_ID) or (user_id in SUDO_USERS):
-        user_is_normal = False
-
-    if user_is_normal:
-        if FSUB_ENABLED is True:
-            member_sub_status = bot.get_chat_member(
-                chat_id=FSUB_CHANNEL_ID,
-                user_id=user_id
-            )
-            if member_sub_status.status not in ["creator", "administrator", "member", "restricted"]:
-                update.effective_message.reply_markdown(
-                    f"*In order to use this bot, you have to be the member of {FSUB_CHANNEL_LINK}.\n\nJoin {FSUB_CHANNEL_LINK} and try using the bot again.*"
-                )
-                return
-    
     if len(args) > 1:
         link = args[1]
         msg = sendMessage(f"Cloning: <code>{link}</code>",context.bot,update)
