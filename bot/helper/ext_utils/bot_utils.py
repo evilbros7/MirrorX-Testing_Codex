fd75d5fd74e718a6a9a92c59a3d5b1041b7378a8
ʝօӄɛʀ <91196108+imvps@users.noreply.github.com>
2021-11-27 20:33:47 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -108,6 +108,7 @@ def get_readable_message():
                         f" & <b>Peers:-</b> {download.aria_download().connections}"
                 except:
                     pass
+                msg += f'\n<b>Adder:</b> {download.message.from_user.first_name} (<code>{download.message.from_user.id}</code>)'
             if download.status() == MirrorStatus.STATUS_DOWNLOADING:
                 msg += f"\n<b>To Stop:-</b> <code>/{BotCommands.CancelMirror} {download.gid()}</code>"                 
             msg += "\n\n"
