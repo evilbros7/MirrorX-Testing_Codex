2b460c3d30d46f1ab463fd2ee31c21acc28f81f7
ʝօӄɛʀ <91196108+imvps@users.noreply.github.com>
2021-11-27 19:48:10 +0530

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -1,4 +1,5 @@
 from telegram.ext import CommandHandler
+from bot import FSUB_ENABLED, FSUB_CHANNEL_ID, FSUB_CHANNEL_LINK, SUDO_USERS, OWNER_ID
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
 from bot.helper.telegram_helper.message_utils import *
@@ -11,6 +12,25 @@ from bot import bot, dispatcher, LOG_UNAME
 
 def cloneNode(update,context):
     args = update.message.text.split(" ",maxsplit=1)
+
+    user_id = update.effective_user.id
+
+    user_is_normal = True
+    if (user_id == OWNER_ID) or (user_id in SUDO_USERS):
+        user_is_normal = False
+
+    if user_is_normal:
+        if FSUB_ENABLED is True:
+            member_sub_status = bot.get_chat_member(
+                chat_id=FSUB_CHANNEL_ID,
+                user_id=user_id
+            )
+            if member_sub_status.status not in ["creator", "administrator", "member", "restricted"]:
+                update.effective_message.reply_markdown(
+                    f"Why don't you join {FSUB_CHANNEL_LINK} first and try using me again?"
+                )
+                return
+
     if len(args) > 1:
         link = args[1]
         msg = sendMessage(f"Cloning: <code>{link}</code>",context.bot,update)
